251030896e52f3c88afd9946e925a6b840308fb6
jun097kim <jun097kim@gmail.com>
2018-10-17 15:57:05 +0900

    백엔드 API 라우트 구성

--- klaytn-backend/src/index.js
+++ klaytn-backend/src/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 const app = express();
 const port = 8000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+app.use('/api', require('./routes/api'));
 
 app.listen(port, () => {
   console.log(`Express listening on port ${port}`);
---
+++ klaytn-backend/src/routes/api/index.js
@@ -0,0 +1,3 @@
+const api = require('express').Router();
+
+module.exports = api;
